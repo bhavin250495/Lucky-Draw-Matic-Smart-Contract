60244b941711e3e035202c12133040a8cbbcb415
Suthar, Bhavin Udavji <suthar_bhavin@network.lilly.com>
2022-01-21 19:00:03 +0530

    Project upload

---
+++ src/App.js
@@ -0,0 +1,155 @@
+import logo from './logo.svg';
+import './App.css';
+import web3 from './web3';
+import lottery from './lottery';
+import React, { Component } from 'react'
+import "./styles.css";
+
+class App extends Component {
+
+  state = {
+    manger: '',
+    players: [],
+    balance: '',
+    value: '',
+    message: '',
+    winners: []
+  };
+
+  // refreshManager = async () => { 
+
+
+  // }
+ 
+
+  async componentDidMount() {
+    const manager = await lottery.methods.manager().call();
+    const players = await lottery.methods.getPlayers().call();
+    const balance = await web3.eth.getBalance(lottery.options.address);
+    const metaMaskAccounts = await web3.eth.getAccounts();
+    const winners = await lottery.methods.getWinners().call();
+    this.setState({ manager, players, balance, metaMaskAccounts, winners });
+  }
+
+  async distribute() {
+    const accounts = await web3.eth.getAccounts();
+    const winners = await lottery.methods.getWinners().call();
+    const firstPrice = 4 / 9 * this.state.balance;
+    const secondPrice = 3 / 9 * this.state.balance;
+    const thirdPrice = 2 / 10 * this.state.balance;
+
+    this.setState({
+      winningPrice: [web3.utils.fromWei(firstPrice.toString(), 'ether'),
+      web3.utils.fromWei(secondPrice.toString(), 'ether'),
+      web3.utils.fromWei(thirdPrice.toString(), 'ether')]
+    });
+    //const we = web3.utils.toBN(firstPrice)
+    await lottery.methods.distributeReward([firstPrice.toString(), secondPrice.toString(), thirdPrice.toString()]).send({ from: accounts[0] });
+  }
+
+  async shouldComponentUpdate(nextProps, nextState) {
+    console.log(this.state.result);
+    if (this.state.result) {
+
+      this.setState({ result: false });
+      const manager = await lottery.methods.manager().call();
+      const players = await lottery.methods.getPlayers().call();
+      const balance = await web3.eth.getBalance(lottery.options.address);
+      this.setState({ manager, players, balance });
+    }
+    return true;
+  }
+ 
+  onSubmit = async (event) => {
+    event.preventDefault();
+    const accounts = await web3.eth.getAccounts();
+    this.setState({ message: 'Waiting on transaction success...' });
+    await lottery.methods.enter().send({
+
+      from: accounts[0],
+      value: web3.utils.toWei(this.state.value, 'ether')
+
+    });
+    this.setState({value:""});
+    this.setState({ result: true });
+    this.setState({ value: '' });
+    this.setState({ message: 'Congrats you have been entered!' });
+
+  }
+
+
+  onClick = async (event) => {
+
+
+    const accounts = await web3.eth.getAccounts();
+    this.setState({ message: 'Waiting on transaction success...' });
+    await lottery.methods.pickWinner().send({
+      from: accounts[0]
+    });
+    const balance = await web3.eth.getBalance(lottery.options.address);
+    this.setState({ balance: balance });
+    this.distribute();
+    // console.log(await lottery.methods.getWinners().call());
+    this.setState({ message: 'Winner has been picked' });
+  }
+
+
+  render() {
+    //web3.eth.getAccounts().then(console.log);
+
+    const playerList = this.state.players.map((item, position) => {
+      return <li>{item}</li>
+    })
+ 
+
+
+    return (
+      <div className="container">
+        <div className="header">
+          <h2>Lottery Contract</h2>
+          <p>Address <a href="https://mumbai.polygonscan.com/address/0x5228ef51DE9f1595e03E0B1AE3eBaDD93aE2692b"  target="_blank" rel="noopener noreferrer" > {lottery.options.address} </a></p>
+          <p>Prize Money {web3.utils.fromWei(this.state.balance, 'ether')} Matic!</p>
+        </div>
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"></link>
+
+        <ul className="w3-ul w3-border">
+          <li><h2>Participants</h2></li>
+          {playerList}
+        </ul>
+        <hr />
+        <hr />
+
+        <form onSubmit={this.onSubmit}>
+
+          <h4>Want to try your luck?</h4>
+          <div>
+            <input required
+              type="text"
+              placeholder="Matic > 0.01"
+              value={this.state.value}
+              onChange={event => this.setState({ value: event.target.value })}
+
+            />
+            <input type="submit" value="Wager" />
+
+          </div>
+          
+        </form>
+        <hr />
+        {this.state.metaMaskAccounts && this.state.manager === this.state.metaMaskAccounts[0] &&
+          <>
+
+            <h4>Ready to pick winner?</h4>
+            <button onClick={this.onClick}>Pick a winner</button>
+          </>}
+        <h1>{this.state.message}</h1>
+      </div>
+
+
+    );
+
+  }
+
+}
+
+export default App;
---
+++ src/lottery.js
@@ -0,0 +1,274 @@
+import web3 from './web3';
+
+//const address = '0xB6BDD688cD539C8aE545cf3E20f6925988d4c760'; //ropsten
+const address = '0x5228ef51DE9f1595e03E0B1AE3eBaDD93aE2692b'; //mumbai
+const abiPoly = [
+  {
+    "inputs": [],
+    "stateMutability": "nonpayable",
+    "type": "constructor"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": true,
+        "internalType": "address",
+        "name": "_from",
+        "type": "address"
+      },
+      {
+        "indexed": false,
+        "internalType": "uint256",
+        "name": "_value",
+        "type": "uint256"
+      }
+    ],
+    "name": "entered",
+    "type": "event"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": false,
+        "internalType": "address",
+        "name": "_to",
+        "type": "address"
+      },
+      {
+        "indexed": false,
+        "internalType": "uint256",
+        "name": "_value",
+        "type": "uint256"
+      }
+    ],
+    "name": "fundsSent",
+    "type": "event"
+  },
+  {
+    "inputs": [],
+    "name": "manager",
+    "outputs": [
+      {
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function",
+    "constant": true
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "name": "players",
+    "outputs": [
+      {
+        "internalType": "address payable",
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function",
+    "constant": true
+  },
+  {
+    "inputs": [],
+    "name": "enter",
+    "outputs": [],
+    "stateMutability": "payable",
+    "type": "function",
+    "payable": true
+  },
+  {
+    "inputs": [],
+    "name": "pickWinner",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256[]",
+        "name": "rewards",
+        "type": "uint256[]"
+      }
+    ],
+    "name": "distributeReward",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "reward",
+        "type": "uint256"
+      },
+      {
+        "internalType": "address payable",
+        "name": "recipent",
+        "type": "address"
+      }
+    ],
+    "name": "sendFund",
+    "outputs": [],
+    "stateMutability": "payable",
+    "type": "function",
+    "payable": true
+  },
+  {
+    "inputs": [],
+    "name": "getPlayers",
+    "outputs": [
+      {
+        "internalType": "address payable[]",
+        "name": "",
+        "type": "address[]"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function",
+    "constant": true
+  },
+  {
+    "inputs": [],
+    "name": "getWinners",
+    "outputs": [
+      {
+        "internalType": "address payable[]",
+        "name": "",
+        "type": "address[]"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function",
+    "constant": true
+  }
+];
+const abi =[
+  {
+    "inputs": [],
+    "stateMutability": "nonpayable",
+    "type": "constructor"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": true,
+        "internalType": "address",
+        "name": "_from",
+        "type": "address"
+      },
+      {
+        "indexed": false,
+        "internalType": "uint256",
+        "name": "_value",
+        "type": "uint256"
+      }
+    ],
+    "name": "entered",
+    "type": "event"
+  },
+  {
+    "inputs": [],
+    "name": "manager",
+    "outputs": [
+      {
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function",
+    "constant": true
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "name": "players",
+    "outputs": [
+      {
+        "internalType": "address payable",
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function",
+    "constant": true
+  },
+  {
+    "inputs": [],
+    "name": "enter",
+    "outputs": [],
+    "stateMutability": "payable",
+    "type": "function",
+    "payable": true
+  },
+  {
+    "inputs": [],
+    "name": "pickWinner",
+    "outputs": [
+      {
+        "internalType": "address[3]",
+        "name": "",
+        "type": "address[3]"
+      }
+    ],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "reward",
+        "type": "uint256"
+      },
+      {
+        "internalType": "address payable",
+        "name": "recipent",
+        "type": "address"
+      }
+    ],
+    "name": "sendReward",
+    "outputs": [],
+    "stateMutability": "payable",
+    "type": "function",
+    "payable": true
+  },
+  {
+    "inputs": [],
+    "name": "getPlayers",
+    "outputs": [
+      {
+        "internalType": "address payable[]",
+        "name": "",
+        "type": "address[]"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function",
+    "constant": true
+  }
+];
+
+export default new web3.eth.Contract(abiPoly,address);
